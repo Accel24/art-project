dad9f4cacc0da98e88b29e358b4f4da01a77721c
Accel24 <46296104+Accel24@users.noreply.github.com>
2019-01-01 14:36:34 +0200

    Update utility.py

--- utility.py
+++ utility.py
@@ -5,4 +5,54 @@ import os
 class utility:
     def google(args):
         os.system("start https://www.google.com/search?q="+args)
+    def password(args):
+
+        passwordExist = None
+
+
+        args = args.lower() 
+        f = open("password.txt","r") 
+        line = f.readline()
+        while line:
+
+            if args in line:
+
+                print(line)
+                passwordExist = True
+               
+                break
+            else:
+
+                passwordExist = False
+
+
+            line = f.readline()
+        if passwordExist == True:
+
+            f.close()
+            line = None
+            read = None
+        else:
+
+            
+            print("Password not found\n")
+            print("Would you like to add it?\n")
+            if input().upper() == "YES":
+
+                print("Working on it")
+                print("What password would you like to assign to account?\n"+ args+"\n")
+
+                password = input()
+                f.close()
+                f = open("password.txt","a")
+                f.write(args + ":" + password+"\n")
+                print("Done")
+                time.sleep(1)
+                print("Reseting...")
+
+            else:
+                
+                print("Understood.Cleaning...")
+                time.sleep(1)
+        
         
