fa797efcc3f866e133dc255553003c3ab7edff03
Accel24 <46296104+Accel24@users.noreply.github.com>
2019-01-01 14:06:16 +0200

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,8 @@
+    
+
+def menu(args):
+    if args.upper().startswith("GOOGLE:"):
+        userDesire = userDesire.split(":")
+        userDesire = userDesire[1]
+        userDesire = userDesire.replace(" ","%20")
+        utility.google(args)
